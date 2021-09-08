2fe8826a9b961ba5d3e651f627c616caba22e577
xxKRASHxx <danil.lisovoy@sigma.sofware>
2021-09-08 18:26:34 +0300

    main > Update functions

---
+++ functions/src/api/index.ts
@@ -0,0 +1,5 @@
+import * as express from 'express';
+
+const api = express()
+
+export default api;
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,9 @@
 import * as functions from "firebase-functions";
+import api from './api';
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
-
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+export const HelloWorld = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", { structuredData: true });
+    response.send("Hello from Firebase!");
 });
+
+export const Api = functions.https.onRequest(api);
